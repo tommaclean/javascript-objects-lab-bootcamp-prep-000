8d208b768e7354bf9a370c4bcea2466bc36f9711
Tom MacLean <tom.j.maclean@gmail.com>
2019-02-17 21:33:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ var updateObjectWithKeyAndValue(object, key, value) = {
   
   return object
 }
+
+
+function updateObjectWithKeyAndValue(targetObject, updatesObject) {
+  return Object.assign({}, object, { key: value} )
+}
