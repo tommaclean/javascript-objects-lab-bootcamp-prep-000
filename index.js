63084bbe5a570a28d08244231fce61da6c85c582
Tom MacLean <tom.j.maclean@gmail.com>
2019-02-17 22:32:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,6 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  return delete object.key
+  object = delete object.key
+  object
 }
